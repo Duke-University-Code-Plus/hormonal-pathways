781c7cd566d216a38f4834f68d98c5f2a5b2e7d3
Karisma Lavana <karismalavana@gmail.com>
2024-06-05 15:47:21 -0400

    13: fixed hormoneserver.py

--- hormoneserver.py
+++ hormoneserver.py
@@ -1,12 +1,16 @@
 import json
 from hormoneModel_6_4_24 import hormoneModel
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/hormonemodel', methods=['GET'])
 def get_arrays():
     return jsonify(hormoneModel())
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(port=5000)
