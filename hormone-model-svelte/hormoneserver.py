a6de7b26cf3c63be0422be0d7bcd5d30872e10d9
BillyMcCune <143638147+BillyMcCune@users.noreply.github.com>
2024-06-06 00:32:46 -0400

    Started work on the back end request impletementation

--- hormone-model-svelte/hormoneserver.py
+++ hormone-model-svelte/hormoneserver.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from hormoneModel_6_4_24 import hormoneModel
+from hormoneMutli import runMutliRun
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -31,6 +32,31 @@ def get_arrays():
         foodShort=float(request.args.get('foodShort', 0)), 
         foodShortbegin=int(request.args.get('foodShortbegin', 0)), 
         foodShortend=int(request.args.get('foodShortend', 0))
+    )
+    return jsonify(results)
+
+@app.route('/multihormonemodel', methods=['GET'])
+def get_multi_arrays():
+    gamma = parse_array_param(request.args.get('gamma'))
+    z = parse_array_param(request.args.get('z'))
+
+    results = runMutliRun(
+        gammaIn=gamma, 
+        GIn=float(request.args.get('G', 0)), 
+        XminIn=float(request.args.get('Xmin', 0)), 
+        delSmaxIn=float(request.args.get('delSmax', 0)), 
+        delCmaxIn=float(request.args.get('delCmax', 0)), 
+        tauIn=float(request.args.get('tau', 0)), 
+        KIn=float(request.args.get('K', 0)), 
+        alphaIn=float(request.args.get('alpha', 0)), 
+        betaIn=float(request.args.get('beta', 0)), 
+        muIn=float(request.args.get('mu', 0)), 
+        zIn=z, 
+        NIn=int(request.args.get('N', 0)), 
+        foodShort=float(request.args.get('foodShort', 0)), 
+        foodShortbegin=int(request.args.get('foodShortbegin', 0)), 
+        foodShortend=int(request.args.get('foodShortend', 0)),
+        numRuns=int(request.args.get('numRuns', 0))                         
     )
     return jsonify(results)
 
