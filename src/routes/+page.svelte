05894cda8785593eaa860aa927f4310bbc704544
Karisma Lavana <karismalavana@gmail.com>
2024-06-11 15:25:20 -0400

    for presentation demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -267,12 +267,22 @@
         <!-- Input fields for parameters with labels -->
         <div class="w-72 m-2">
             <div class="relative w-full min-w-[200px] h-10">
-                <label for="Gamma" class="form-label">Gamma</label>
                 <input
                     id="Gamma"
-                    class="form-input"
+                    class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 focus:border-purple-500 placeholder:text-blue-gray-100"
                     bind:value={$gamma}
                 />
+                <label for="Gamma" class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate
+                peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent
+                peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px]
+                peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px]
+                before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2
+                before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent
+                after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1
+                peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2
+                after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent
+                peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-purple-500 before:border-blue-gray-200
+                peer-focus:before:!border-purple-500 after:border-blue-gray-200 peer-focus:after:!border-purple-500">Gamma</label>
             </div>
         </div>
 
@@ -290,18 +300,18 @@
 
 
 
-        <FormInput
+        <!-- <FormInput
             id="G"
             type="number"
             min="0"
             max="1"
             step="0.1"
-        />
+        /> -->
 
 
 
 
-        <!-- <div class="w-72 m-2">
+        <div class="w-72 m-2">
             <div class="relative w-full min-w-[200px] h-10">
                 <input
                     id="G"
@@ -328,7 +338,7 @@
                     >G</label
                 >
             </div>
-        </div> -->
+        </div>
 
 
 
