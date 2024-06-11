334ae2e6f07653aea7170064287ba5be7ecbad37
Vanessa Lin <vl69@duke.edu>
2024-06-11 10:58:51 -0400

    Created sliders for non-array parameters, need to work on clearing canvas if filled

--- frontend-tools/src/routes/+page.svelte
+++ frontend-tools/src/routes/+page.svelte
@@ -1,123 +1,3 @@
-<head>
-    <!-- Responsive layout  -->
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <style>
-        /* Container for slider, acts as a template  */
-        .slidecontainer {
-            display: flex; /* allows elements to spread evenly */
-            flex-wrap: wrap; /* wrap to another line if not enough space */
-            justify-content: space-between;
-            width: 100%;
-            padding: 10px;
-        }
-
-        /* Slider */
-        .slider {
-            -webkit-appearance: none; /* No default browser styling */
-            width: 50%;
-            height: 10px;
-            background: #d3d3d3; /* Slider color */
-            outline: none;
-            opacity: 0.7;
-            -webkit-transition: 0.2s; /* Allows it to be smoother*/
-            transition: opacity 0.2s;
-        }
-
-        /* Slidding part, styling for WebKit-based browsers such as Safari and Chrome */
-        .slider::-webkit-slider-thumb {
-            -webkit-appearance: none;
-            appearance: none;
-            width: 25px;
-            height: 25px;
-            background: #04aa6d;
-            cursor: pointer; /* Change cursor to pointer when hovering */
-        }
-
-        /* Also slidding part but for styling for Mozilla-based browsers such as Firefox */
-        .slider::-moz-range-thumb {
-            width: 25px;
-            height: 25px;
-            background: #04aa6d;
-            cursor: pointer;
-        }
-    </style>
-</head>
-
-<body>
-    <h1 class="text-center text-2xl font-serif font-bold p-4">
-        Hormonal Pathways
-        
-    </h1>
-    
-
-    <!-- First row of sliders -->
-    <div class="slidecontainer">
-        <div>
-            <label for="GIn_slider">GIn: </label>  <!-- Label for slider -->
-            <input
-                type="range"
-                min="0.0"
-                max="0.5"
-                step="0.1"
-                value="0.1"
-                class="slider"
-                id="GIn_slider"
-            />
-            <span id="GIn_output">0.1</span> <!-- Label for value of slider -->
-        </div>
-        
-        <div>
-            <label for="XminIn_slider">XminIn: :</label>
-            <input
-                type="range"
-                min="0"
-                max="5"
-                step="1"
-                value="1"
-                class="slider"
-                id="XminIn_slider"
-            />
-            <span id="XminIn_output">1</span>
-        </div>
-    </div>
-
-
-    <script>
-        // Update slider values
-        function updateSliderValue(sliderID, outputID) {
-            var slider = document.getElementById(sliderID); //retrieves slider
-            var output = document.getElementById(outputID); //retrieves output
-            output.innerHTML = slider.value; //sets inner HTML content of output element to value of slider
-
-            slider.oninput = function () { //event: when slider value changes, function is executed
-                output.innerHTML = this.value; //this refers to slider element that triggered the event 
-            };
-        }
-
-        // Update values for each slider
-        updateSliderValue("GIn_slider", "GIn_output");
-        updateSliderValue("XminIn_slider", "XminIn_output");
-    </script>
-</body>
-
-<div class="p-8">
-    <canvas id="Xhist"></canvas>
-    <!-- Energy -->
-    <canvas id="Shist_1"></canvas>
-    <!-- Sensitivity 1 -->
-    <canvas id="Shist_2"></canvas>
-    <!-- Sensitivity 2 -->
-    <canvas id="Shist_3"></canvas>
-    <!-- Sensitivity 3 -->
-    <canvas id="Chist"></canvas>
-    <!-- Concentration -->
-    <canvas id="Whist"></canvas>
-    <!-- Fitness -->
-    <canvas id="Wcuml"></canvas>
-    <!-- Cumulative Fitness -->
-</div>
-
 <script>
     import { onMount } from "svelte";
     import axios from "axios";
@@ -128,28 +8,30 @@
     let GIn = 0.1;
     let XminIn = 1;
     let delSmaxIn = 1;
-    let delCmaxIn = 1; 
+    let delCmaxIn = 1;
     let tauIn = 5;
     let KIn = 1;
     let alphaIn = 2;
     let betaIn = 2;
-    let muIn = 0.0001; 
-    let NIn = 10; 
-    let foodShort = 1.5;  
-    let foodShortbegin = 50; 
-    let foodShortend = 75; 
-
+    let muIn = 0.0001;
+    let NIn = 100;
+    let foodShort = 0.5;
+    let foodShortbegin = 8;
+    let foodShortend = 20;
 
     async function fetchData() {
-    try {
-      const response = await axios.get(`api/test?GIn=${GIn}&XminIn=${XminIn}&delSmaxIn=${delSmaxIn}&delCmaxIn=${delCmaxIn}&tauIn=${tauIn}&KIn=${KIn}&alphaIn=${alphaIn}&betaIn=${betaIn}&muIn=${muIn}&NIn=${NIn}&foodShort=${foodShort}&foodShortbegin=${foodShortbegin}&foodShortend=${foodShortend}`);
-      responseData = response.data;
-      console.log("responseData: ", responseData);
-      Charts();
-    } catch (error) {
-      console.error("Error:", error);
+        try {
+            const response = await axios.get(
+                `api/test?GIn=${GIn}&XminIn=${XminIn}&delSmaxIn=${delSmaxIn}&delCmaxIn=${delCmaxIn}&tauIn=${tauIn}&KIn=${KIn}&alphaIn=${alphaIn}&betaIn=${betaIn}&muIn=${muIn}&NIn=${NIn}&foodShort=${foodShort}&foodShortbegin=${foodShortbegin}&foodShortend=${foodShortend}`,
+            );
+            responseData = response.data;
+            console.log("responseData: ", responseData);
+            Charts();
+        } catch (error) {
+            console.error("Error:", error);
+        }
     }
-  }
+
     function Charts() {
         createChart(
             "Xhist",
@@ -196,7 +78,7 @@
     }
 
     function createChart(canvas, chartTitle, y, color) {
-        var ctx = document.getElementById(canvas);
+        var ctx = document.getElementById(canvas).getContext("2d");
         var x = Array.from({ length: y.length }, (_, i) => i + 1);
         new Chart(ctx, {
             type: "line",
@@ -213,36 +95,302 @@
             },
         });
     }
-    function updateCharts() {
-    console.log("Updating charts with responseData:", responseData);
-    if (Chist) {
-      Chist.destroy();
-    }
-    const ctx = document.getElementById('Chist');
-    Chist = createChart(ctx, 'Concentration', Object.values(responseData), 'rgba(75, 192, 192, 1)');
-  }
-
-
-//function increment(event) {  NIn += 1;}
-
 </script>
 
-<label>
-    GIn: {GIn}
-    <input type="range" min="0" max="10" bind:value={GIn} >
-  </label>
-<button on:click={fetchData}>Fetch Data</button>
-<button on:click={updateCharts}>Update Charts</button>
+<head>
+    <style>
+        /* Container for slider, acts as a template  */
+        .slidecontainer {
+            display: flex; /* allows elements to spread evenly */
+            flex-wrap: wrap; /* wrap to another line if not enough space */
+            justify-content: space-between;
+            width: 100%;
+            padding: 10px;
+        }
 
+        /* Slider */
+        .slider {
+            -webkit-appearance: none; /* No default browser styling */
+            width: 50%;
+            height: 10px;
+            background: #d3d3d3; /* Slider color */
+            outline: none;
+            opacity: 0.7;
+            -webkit-transition: 0.2s; /* Allows it to be smoother*/
+            transition: opacity 0.2s;
+        }
 
+        /* Slidding part, styling for WebKit-based browsers such as Safari and Chrome */
+        .slider::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 25px;
+            height: 25px;
+            background: #04aa6d;
+            cursor: pointer; /* Change cursor to pointer when hovering */
+        }
 
+        /* Also slidding part but for styling for Mozilla-based browsers such as Firefox */
+        .slider::-moz-range-thumb {
+            width: 25px;
+            height: 25px;
+            background: #04aa6d;
+            cursor: pointer;
+        }
 
+        .button {
+            background-color: white;
+            border: 2px solid pink;
+            color: black;
+            padding: 10px 10px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 10px 10px;
+            cursor: pointer;
+        }
+    </style>
+</head>
 
+<body>
+    <h1 class="text-center text-2xl font-serif font-bold p-4">
+        Hormonal Pathways
+    </h1>
 
+    <!-- First row of sliders -->
+    <div class="slidecontainer">
+        <div>
+            <label for="GIn_slider">GIn: </label>
+            <!-- Label for slider -->
+            <input
+                type="range"
+                min="0.0"
+                max="0.5"
+                step="0.1"
+                bind:value={GIn}
+                class="slider"
+                id="GIn_slider"
+            />
+            <span id="GIn_output">0.1</span>
+            <!-- Label for value of slider -->
+        </div>
 
+        <div>
+            <label for="XminIn_slider">XminIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={XminIn}
+                class="slider"
+                id="XminIn_slider"
+            />
+            <span id="XminIn_output">1</span>
+        </div>
 
+        <div>
+            <label for="delSmaxIn_slider">delSmaxIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={delSmaxIn}
+                class="slider"
+                id="delSmaxIn_slider"
+            />
+            <span id="delSmaxIn_output">1</span>
+        </div>
 
+        <div>
+            <label for="delCmaxIn_slider">delCmaxIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={delCmaxIn}
+                class="slider"
+                id="delCmaxIn_slider"
+            />
+            <span id="delCmaxIn_output">1</span>
+        </div>
 
+        <div>
+            <label for="tauIn_slider">tauIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={tauIn}
+                class="slider"
+                id="tauIn_slider"
+            />
+            <span id="tauIn_output">5</span>
+        </div>
 
+        <div>
+            <label for="KIn_slider">KIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={KIn}
+                class="slider"
+                id="KIn_slider"
+            />
+            <span id="KIn_output">1</span>
+        </div>
 
+        <div>
+            <label for="alphaIn_slider">alphaIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={alphaIn}
+                class="slider"
+                id="alphaIn_slider"
+            />
+            <span id="alphaIn_output">2</span>
+        </div>
 
+        <div>
+            <label for="betaIn_slider">betaIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="5"
+                step="1"
+                bind:value={betaIn}
+                class="slider"
+                id="betaIn_slider"
+            />
+            <span id="betaIn_output">2</span>
+        </div>
+
+        <div>
+            <label for="muIn_slider">muIn: :</label>
+            <input
+                type="range"
+                min="0"
+                max="0.0005"
+                step="0.0001"
+                bind:value={muIn}
+                class="slider"
+                id="muIn_slider"
+            />
+            <span id="muIn_output">0.0001</span>
+        </div>
+
+        <div>
+            <label for="NIn_slider">NIn: :</label>
+            <input
+                type="range"
+                min="100"
+                max="200"
+                step="20"
+                bind:value={NIn}
+                class="slider"
+                id="NIn_slider"
+            />
+            <span id="NIn_output">100</span>
+        </div>
+
+        <div>
+            <label for="foodShort_slider">foodShort: </label>
+            <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.1"
+                bind:value={foodShort}
+                class="slider"
+                id="foodShort_slider"
+            />
+            <span id="foodShort_output">0.5</span>
+        </div>
+
+        <div>
+            <label for="foodShortbegin_slider">foodShortbegin: </label>
+            <input
+                type="range"
+                min="0"
+                max="50"
+                step="1"
+                bind:value={foodShortbegin}
+                class="slider"
+                id="foodShortbegin_slider"
+            />
+            <span id="foodShortbegin_output">8</span>
+        </div>
+
+        <div>
+            <label for="foodShortend_slider">foodShortend: </label>
+            <input
+                type="range"
+                min="0"
+                max="150"
+                step="1"
+                bind:value={foodShortend}
+                class="slider"
+                id="foodShortend_slider"
+            />
+            <span id="foodShortend_output">20</span>
+        </div>
+
+    </div>
+
+    <button class="button" on:click={fetchData}>Run Simulation</button>
+
+    <script>
+        // Update slider values
+        function updateSliderValue(sliderID, outputID) {
+            var slider = document.getElementById(sliderID); //retrieves slider
+            var output = document.getElementById(outputID); //retrieves output
+            output.innerHTML = slider.value; //sets inner HTML content of output element to value of slider
+
+            slider.oninput = function () {
+                //event: when slider value changes, function is executed
+                output.innerHTML = this.value; //this refers to slider element that triggered the event
+            };
+        }
+
+        // Update values for each slider
+        updateSliderValue("GIn_slider", "GIn_output");
+        updateSliderValue("XminIn_slider", "XminIn_output");
+        updateSliderValue("delSmaxIn_slider", "delSmaxIn_output");
+        updateSliderValue("delCmaxIn_slider", "delCmaxIn_output");
+        updateSliderValue("tauIn_slider", "tauIn_output");
+        updateSliderValue("KIn_slider", "KIn_output");
+        updateSliderValue("alphaIn_slider", "alphaIn_output");
+        updateSliderValue("betaIn_slider", "betaIn_output");
+        updateSliderValue("muIn_slider", "muIn_output");
+        updateSliderValue("NIn_slider", "NIn_output");
+        updateSliderValue("foodShort_slider", "foodShort_output");
+        updateSliderValue("foodShortbegin_slider", "foodShortbegin_output");
+        updateSliderValue("foodShortend_slider", "foodShortend_output");
+    </script>
+</body>
+
+<div class="p-8">
+    <canvas id="Xhist"></canvas>
+    <!-- Energy -->
+    <canvas id="Shist_1"></canvas>
+    <!-- Sensitivity 1 -->
+    <canvas id="Shist_2"></canvas>
+    <!-- Sensitivity 2 -->
+    <canvas id="Shist_3"></canvas>
+    <!-- Sensitivity 3 -->
+    <canvas id="Chist"></canvas>
+    <!-- Concentration -->
+    <canvas id="Whist"></canvas>
+    <!-- Fitness -->
+    <canvas id="Wcuml"></canvas>
+    <!-- Cumulative Fitness -->
+</div>
