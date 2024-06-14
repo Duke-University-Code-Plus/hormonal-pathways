af89fabd90aeef679ff7cfd1922f22285513b9e0
Vanessa Lin <vl69@duke.edu>
2024-06-14 11:12:02 -0400

    Added input next to sliders

--- src/routes/multimodel/Nested/SliderInput.svelte
+++ src/routes/multimodel/Nested/SliderInput.svelte
@@ -1,32 +1,42 @@
 <script>
     export let id;
-    export let inputType;
     export let min;
     export let max;
     export let step;
-    export let inputVar; 
-  
-  </script>
-  
-  <div class="w-72 m-2">
+    export let inputVar;
+</script>
+
+<div class="w-72 m-2">
     <div class="relative w-full min-w-[200px] h-10">
         <input
-          id={id}
-          type="range"
-          min={min}
-          max={max}
-          step={step}
-          class="peer w-full h-full bg-transparent font-sans text-sm appearance-none [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-pink-100 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[20px] [&::-webkit-slider-thumb]:w-[20px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-indigo-500"
-          bind:value={inputVar}
+            {id}
+            type="range"
+            {min}
+            {max}
+            {step}
+            class="peer w-3/4 h-full bg-transparent font-sans text-sm appearance-none [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-gray-100 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[20px] [&::-webkit-slider-thumb]:w-[20px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+            bind:value={inputVar}
         />
-        
+
+        <input
+            {id}
+            type="number"
+            {min}
+            {max}
+            {step}
+            class="peer w-1/4 h-3/4 bg-transparent text-blue-gray-700 font-sans font-normal outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 
+            border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 focus:border-purple-500 placeholder:text-blue-gray-100 absolute text-[11px] -mr-1 bottom-0 right-0 -top-0.5"
+            bind:value={inputVar}
+        />
+
         <label
             for={id}
-            class="flex absolute left-0 font-normal truncate leading-tight -top-1.5 text-[11px]">{id}
+            class="flex absolute left-0 font-normal truncate leading-tight -top-1.5 text-[11px]"
+            >{id}
         </label>
-        
-        <span class="absolute text-[11px] -mr-1 bottom-0  right-0 -top-1.5 -mb-6">{inputVar}</span>
 
-      
+        <!--
+        <span class="absolute text-[11px] -mr-1 bottom-0  right-0 -top-1.5 -mb-6">{inputVar}</span>
+        -->
     </div>
-  </div>
+</div>
