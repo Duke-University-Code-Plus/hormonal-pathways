5e14b605ff36fe8d1ac190cf367da384e74f05dc
DootDoot4567 <dootdoot4567@gmail.com>
2024-06-20 15:36:53 -0400

    changed the position of modal button

--- src/routes/multimodel/Nested/FormInput.svelte
+++ src/routes/multimodel/Nested/FormInput.svelte
@@ -13,6 +13,30 @@
 </script>
 
 <div class="w-72 m-2">
+  <div class="flex flex-row flex-wrap justify-end ">
+    <button on:click={() => (showModal = true)}> 
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" size-5 -mt-px ">
+        <path fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+          clip-rule="evenodd">
+        </path>
+      </svg> 
+    </button>
+
+    <Modal bind:showModal>
+      <h2 
+        class="m-top-5 mt-3 text-blue-gray-500" 
+        slot="header">
+        {id}
+      </h2>
+      <div class="flex ms-auto inline-flex m-8">
+        <p class="font-sans text-sm font-normal text-blue-gray-500">
+          {modalMessage}
+        </p>
+      </div>
+    </Modal>
+  </div>
+
   <div class="relative w-full min-w-[200px] h-10">
     <!--{#if inputType === 'number'}-->
     <!--Old Styling Code-->
@@ -53,27 +77,6 @@
       class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-900 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-gray-500 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-gray-500 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-purple-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-purple-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-purple-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:after:border-t-transparent peerpeer-disabled:peer-placeholder-shown:text-blue-gray-500"
     >{id}</label>
   </div>
-  <button on:click={() => (showModal = true)}> 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 -mt-px">
-      <path fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd">
-      </path>
-    </svg> 
-  </button>
-
-  <Modal bind:showModal>
-    <h2 
-      class="m-top-5 mt-3 text-blue-gray-500" 
-      slot="header">
-      {id}
-    </h2>
-    <div class="flex ms-auto inline-flex m-8">
-      <p class="font-sans text-sm font-normal text-blue-gray-500">
-        {modalMessage}
-      </p>
-    </div>
-  </Modal>
 </div>
 
 
