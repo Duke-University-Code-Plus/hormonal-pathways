4b0a93a8c471816ad5ef80ef8e9cdef5f224605a
DootDoot4567 <dootdoot4567@gmail.com>
2024-07-08 16:06:57 -0400

    changed the position of tool tip icon on sliders

--- src/routes/Nested/SliderInput.svelte
+++ src/routes/Nested/SliderInput.svelte
@@ -12,9 +12,9 @@
 </script>
 
 <div class="w-72 m-2">
-    <div class="flex flex-row flex-wrap justify-end ">
-        <button on:click={() => (showModal = true)}> 
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" size-5 -mt-px ">
+    <div class="flex flex-row flex-wrap justify-end mr-[70px] -mt-[16px]">
+        <button class="mt-4" on:click={() => (showModal = true)}> 
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class=" size-5 -mt-px">
             <path fill-rule="evenodd"
               d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
               clip-rule="evenodd">
@@ -43,7 +43,7 @@
             {min}
             {max}
             {step}
-            class="peer w-3/4 h-full bg-transparent font-sans text-sm appearance-none [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-gray-100 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[20px] [&::-webkit-slider-thumb]:w-[20px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+            class="peer w-3/4 h-full bg-transparent font-sans text-sm appearance-none [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-gray-100 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[20px] [&::-webkit-slider-thumb]:w-[20px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500 -mt-2"
             bind:value={inputVar}
         />
 
@@ -55,14 +55,14 @@
             {max}
             {step}
             class="peer w-1/4 h-3/4 bg-transparent text-blue-gray-700 font-sans font-normal outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200
-            border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 focus:border-purple-500 placeholder:text-blue-gray-100 absolute text-[11px] -mr-1 bottom-0 right-0 -top-0.5"
+            border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 focus:border-purple-500 placeholder:text-blue-gray-100 absolute text-[11px] -mr-1 bottom-0 right-0 -top-0.5 -mt-2"
             bind:value={inputVar}
         />
 
         <!-- Variable Label -->
         <label
             for={id}
-            class="flex absolute left-0 font-normal truncate leading-tight -top-1.5 text-[11px]"
+            class="flex absolute left-0 font-normal truncate leading-tight -top-1.5 text-[11px] -mt-2"
             >{id}
         </label>
 
