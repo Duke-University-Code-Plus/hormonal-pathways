b84b15f73023ef7eb1e93a392ae791e2a2161e8d
DootDoot4567 <dootdoot4567@gmail.com>
2024-07-12 22:45:22 -0400

    deleted old code

--- src/routes/Nested/ExportChart.svelte
+++ src/routes/Nested/ExportChart.svelte
@@ -51,14 +51,16 @@
                 <p class="font-sans text-sm font-normal text-blue-gray-500">
                     {#if isLoading}
                         <div role="status">
-                            <svg aria-hidden="true" class="flex justify-cetner ml-8 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg aria-hidden="true" class="flex ml-8 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-indigo-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                             </svg>
                             <span class="sr-only">Loading...</span>
                         </div>
                     {:else}
-                        {modalMessage}
+                        <div class="">
+                            {modalMessage}
+                        </div>
                     {/if}
                 </p>
             </div>
@@ -68,50 +70,3 @@
 </div>
 
 
-
-
-<!-- <script>
-    import Chart from "chart.js/auto";
-    import axios from "axios";
-    import Modal from './Modal.svelte';
-    import { get } from 'svelte/store';
-    import { base64Data } from '../base64_store.js';
-
-    function runLlavaScript() {
-        const base64 = get(base64Data);  // Get the base64 data from the Svelte store
-        console.log("Base64 data:", base64);
-        // Use the base64 data to run your Python file
-    }
-
-    runLlavaScript();    
-
-    export let data;
-
-    let modalMessage = null;
-
-    let showModal = false;
-
-
-
-</script>
-
-<div class="flex flex-row flex-wrap justify-end -mt-3 ml-1">
-    <button on:click={() => (showModal = true) }> 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
-        </svg>          
-    </button>
-
-    <Modal bind:showModal>
-      <h2 
-        class="m-top-5 mt-3 text-blue-gray-500" 
-        slot="header">
-        {id}
-      </h2>
-      <div class="flex ms-auto inline-flex m-8">
-        <p class="font-sans text-sm font-normal text-blue-gray-500">
-          Useful information
-        </p>
-      </div>
-    </Modal>
-</div> -->
