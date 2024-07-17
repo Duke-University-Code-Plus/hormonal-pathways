ee83c4fa964832cd1f1e96b8b008f17b484738eb
BillyMcCune <billymccune14@gmail.com>
2024-07-17 13:42:08 -0400

    changed nav to have logo and changed the graph y label in multi

--- src/routes/multimodel/+page.svelte
+++ src/routes/multimodel/+page.svelte
@@ -190,7 +190,7 @@
                 },
                 y: {
                     beginAtZero: true,
-                    title: { display: true, text: "y label" },
+                    title: { display: true},
                     //max: maxValue
                 },
             },
